b8dbabee77feb24b0865a7e8ee87bb21657166a5
RevengeNinja268 <85186928+RevengeNinja268@users.noreply.github.com>
2021-06-01 13:41:24 -0400

    Update and rename GenPass-1.3.0.py to GenPass-1.3.7.py

--- GenPass-1.3.7.py
+++ GenPass-1.3.7.py
@@ -1,6 +1,7 @@
-import string, random
+import string, random, sys
 
 print("Welcome to GenPass\n")
+print("You are currently using GenPass 1.3.0\n")
 
 def gen_password(length):
     password = ''
@@ -16,15 +17,19 @@ def gen_password(length):
     
 try:
     print('It is recommend to use 8+ characters in a password, but it is up to you')
-    dl = int(input('How many letters do you need in your password: '))
+    dl = int(input('How many letters do you want in your password: ').strip())
     if dl < 8:
         confirmed = input('You have selected a length below the recommended length. Are you sure you want to continue? [y/n]: ')
         if confirmed.lower().strip() == 'n':
-            quit()
-            
+            sys.exit("Thanks for using GenPass.")
 except ValueError:
-    print('Enter an integer value!')
-    quit()
-
-print("\nHere are 5 possible passwords!\n")
-print("Thanks for using GenPass")
+    sys.exit("Enter an appropriate value!")
+    
+try: 
+    num_of_pwords = int(input('How many passwords do you want: ').strip())
+except: 
+    sys.exit("Enter an appropriate value!")
+print("\nHere are your possible passwords:\n")
+for i in range(num_of_pwords):
+    print(gen_password(dl) + '\n')
+print("Thanks for using GenPass.")
